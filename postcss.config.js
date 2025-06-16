aa12ff57c1c04b9e7250d4f0750d1179a076e4d5
N.E.T <143912452+TarruckWheeler@users.noreply.github.com>
2025-06-16 11:40:57 -0400

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// BRIDGES UNESCO 3.0 - Created by Tarruck Wheeler (tarruck@stanford.edu)
+// PostCSS Configuration for UNESCO Intercultural Leadership Platform
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
